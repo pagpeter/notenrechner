6b5f6d651fa30dca9ce86a1fdbf1261b882ee3f7
Peet <gh@peet.ws>
2023-06-25 01:01:42 +0200

    v1

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,59 +1,102 @@
 <script>
-	import Counter from './Counter.svelte';
-	import welcome from '$lib/images/svelte-welcome.webp';
-	import welcome_fallback from '$lib/images/svelte-welcome.png';
+  // @ts-nocheck
+
+  import { onMount } from "svelte";
+  let ls = null;
+
+  onMount(() => {
+    ls = localStorage;
+
+    if (ls.grades) {
+      grades = JSON.parse(ls.grades);
+      console.log(ls, ls.grades);
+    } else
+      grades = [
+        { name: "Deutsch", grade: 15, weight: 1, id: 0 },
+        { name: "Mathe", grade: 15, weight: 1, id: 1 },
+        { name: "Englisch", grade: 15, weight: 1, id: 2 },
+      ];
+  });
+
+  /**
+   * @type {any[]}
+   */
+  export let grades = [];
+
+  let idCounter = 99;
+
+  function addGrade() {
+    const newGrade = {
+      name: "-",
+      grade: 15,
+      weight: 1,
+      id: idCounter++,
+    };
+    grades = [...grades, newGrade];
+  }
+
+  /**
+   * @type {Any}
+   */
+  $: points = (
+    grades.reduce((acc, g) => acc + (g.grade - 0) * g.weight, 0) /
+    grades.reduce((acc, g) => acc + g.weight, 0)
+  ).toFixed(2);
+
+  $: ls && ls.setItem("grades", JSON.stringify(grades));
+
+  $: grade = ((17 - points) / 3).toFixed(2);
 </script>
 
 <svelte:head>
-	<title>Home</title>
-	<meta name="description" content="Svelte demo app" />
+  <title>Home</title>
+  <meta name="description" content="Svelte demo app" />
 </svelte:head>
 
 <section>
-	<h1>
-		<span class="welcome">
-			<picture>
-				<source srcset={welcome} type="image/webp" />
-				<img src={welcome_fallback} alt="Welcome" />
-			</picture>
-		</span>
+  <h1 class="font-black text-2xl p-5">Notenrechner</h1>
 
-		to your new<br />SvelteKit app
-	</h1>
+  <div class="p-5">
+    <ul class="p-3 w-50 bg-slate-300 rounded shadow">
+      {#each grades as { name, grade, weight, id }}
+        <li
+          class="flex justify-between items-center gap-2 bg-gray-200 p-2 rounded"
+        >
+          <p>
+            <input
+              class="p-1 rounded bg-gray-200 outline outline-slate-200"
+              bind:value={name}
+            />
+          </p>
+          <p>
+            <input
+              class="p-1 rounded bg-gray-200"
+              type="number"
+              max="15"
+              min="0"
+              bind:value={grade}
+            /> pt.
+          </p>
+          <p>
+            <input
+              class="p-1 rounded bg-gray-200 w-8"
+              type="number"
+              bind:value={weight}
+              min="0"
+            />x
+          </p>
+          <button
+            on:click={() => (grades = grades.filter((g) => g.id !== id))}
+            class="text-sm text-red-500 cursor-pointer">X</button
+          >
+        </li>
+      {/each}
 
-	<h2>
-		try editing <strong>src/routes/+page.svelte</strong>
-	</h2>
+      <button class="p-2 text-xl" on:click={addGrade}>+</button>
+    </ul>
 
-	<Counter />
+    <p class="py-2">
+      Durchschnitt: <b>{points}</b> Punkte (Schnitt: <b>{grade}</b>)
+    </p>
+  </div>
 </section>
-
-<style>
-	section {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		flex: 0.6;
-	}
-
-	h1 {
-		width: 100%;
-	}
-
-	.welcome {
-		display: block;
-		position: relative;
-		width: 100%;
-		height: 0;
-		padding: 0 0 calc(100% * 495 / 2048) 0;
-	}
-
-	.welcome img {
-		position: absolute;
-		width: 100%;
-		height: 100%;
-		top: 0;
-		display: block;
-	}
-</style>
